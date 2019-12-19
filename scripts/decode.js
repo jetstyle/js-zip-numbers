1987410d7bd595dd46069264afc2343ba38ce91f
bukina.e <ecfhf567>
2019-12-19 19:26:57 +0500

    fix files with eslint

--- scripts/decode.js
+++ scripts/decode.js
@@ -4,7 +4,7 @@ const DELTA_LIST_BY_TWO = ':';
 const ZIP_START_DELIMITER = '(';
 const ZIP_END_DELIMITER = ')';
 
-const intBase = 10;
+let intBase = 10;
 
 /**
  * Parse string to array of encoding numbers
@@ -12,32 +12,27 @@ const intBase = 10;
  * @param string
  * @returns {[]|*[]}
  */
-const decode = string => {
-    if (!string) {
-        return [];
-    }
-    let items;
+const decode = (string) => {
+  if (!string) {
+    return [];
+  }
+  let items;
 
-    var intBase = 10;
+  // Parse base for int
+  if (string.startsWith('x')) {
+    let base;
+    [base, string] = string.split('x');
+    intBase = base.slice(1);
+  }
 
-    // Parse base for int
-    if (string.startsWith('x')) {
-        let base;
-        [base, string] = string.split('x');
-        intBase = base.slice(1);
-    }
+  // Parse empty string as empty list
+  if (string.startsWith('~')) {
+    items = parseDelta(string.slice(1));
+  } else {
+    items = parseString(string);
+  }
 
-    // Parse empty string as empty list
-    if (string.startsWith('~')) {
-        items = parseDelta(string.slice(1));
-        console.log(items);
-    }
-    else {
-        items = parseString(string);
-        console.log(items)
-    }
-
-    return items;
+  return items;
 };
 
 /**
@@ -46,28 +41,28 @@ const decode = string => {
  * @param string
  * @return {[]}
  */
-const parseString = string => {
-    let buff = '', tokens = [], zipBuff = [];
+const parseString = (string) => {
+  let buff = ''; const tokens = []; const
+    zipBuff = [];
 
-    for (const ltr of string) {
-        if (ltr === ZIP_START_DELIMITER) zipBuff.push(1);
-        if (ltr === ZIP_END_DELIMITER) zipBuff.pop();
-        if (zipBuff.length === 0 && ltr === NUM_DELIMITER) {
-            parseToken(buff).forEach((item) => {
-                tokens.push(item);
-            });
-            buff = '';
-        }
-        else buff += ltr;
-    }
+  for (const ltr of string) {
+    if (ltr === ZIP_START_DELIMITER) zipBuff.push(1);
+    if (ltr === ZIP_END_DELIMITER) zipBuff.pop();
+    if (zipBuff.length === 0 && ltr === NUM_DELIMITER) {
+      parseToken(buff).forEach((item) => {
+        tokens.push(item);
+      });
+      buff = '';
+    } else buff += ltr;
+  }
 
-    if (buff) {
-        parseToken(buff).forEach((item) => {
-            tokens.push(item);
-        });
-    }
+  if (buff) {
+    parseToken(buff).forEach((item) => {
+      tokens.push(item);
+    });
+  }
 
-    return tokens;
+  return tokens;
 };
 
 
@@ -77,26 +72,25 @@ const parseString = string => {
  * @param token
  * @return {[]} array with tokens
  */
-const parseToken = token => {
-    let tokens = [];
-    if (token.indexOf(ZIP_START_DELIMITER) > -1) {
-        let [base, subString] = token.split(ZIP_START_DELIMITER);
-        base = parseInt(base, 10);
-        let items = parseString(subString.slice(0, subString.length-1));
-        items.forEach((item) => tokens.push(item+base));
-        return tokens;
-    }
-    if (token.indexOf('-') > -1) {
-        let [start, stop] = token.split('-');
-        start = parseInt(start, intBase);
-        stop = parseInt(stop, intBase);
-
-        for (let i = start; i <= stop; i += 1) {
-            tokens.push(i);
-        }
-    }
-    else tokens = [parseInt(token, intBase)];
+const parseToken = (token) => {
+  let tokens = [];
+  if (token.indexOf(ZIP_START_DELIMITER) > -1) {
+    let [base, subString] = token.split(ZIP_START_DELIMITER);
+    base = parseInt(base, 10);
+    const items = parseString(subString.slice(0, subString.length - 1));
+    items.forEach((item) => tokens.push(item + base));
     return tokens;
+  }
+  if (token.indexOf('-') > -1) {
+    let [start, stop] = token.split('-');
+    start = parseInt(start, intBase);
+    stop = parseInt(stop, intBase);
+
+    for (let i = start; i <= stop; i += 1) {
+      tokens.push(i);
+    }
+  } else tokens = [parseInt(token, intBase)];
+  return tokens;
 };
 
 /**
@@ -105,19 +99,19 @@ const parseToken = token => {
  * @param string
  * @return {[]} array with tokens
  */
-const parseDelta = string => {
-    let tokens = [],
-        chunks = deltaChunks(string);
+const parseDelta = (string) => {
+  let tokens = [];
+  const chunks = deltaChunks(string);
 
-    chunks.forEach((chunk) => tokens = tokens.concat(parseDeltaChunks(chunk)));
+  chunks.forEach((chunk) => tokens = tokens.concat(parseDeltaChunks(chunk)));
 
-    let last = 0;
-    tokens.forEach((token, i) => {
-        tokens[i]=token+last;
-        last=tokens[i];
-    });
+  let last = 0;
+  tokens.forEach((token, i) => {
+    tokens[i] = token + last;
+    last = tokens[i];
+  });
 
-    return tokens;
+  return tokens;
 };
 
 
@@ -127,60 +121,38 @@ const parseDelta = string => {
  * @param chunk
  * @return {[]} array with tokens
  */
-const parseDeltaChunks = chunk => {
-    let listBy, blocks, tokens = [];
-    if (chunk.startsWith(DELTA_LIST_BY_ONE)) listBy = 1;
-    if (chunk.startsWith(DELTA_LIST_BY_TWO)) listBy = 2;
-    if (listBy) chunk = chunk.slice(1);
-    blocks = chunk.split('x');
+const parseDeltaChunks = (chunk) => {
+  let listBy;
+  let tokens = [];
+  if (chunk.startsWith(DELTA_LIST_BY_ONE)) listBy = 1;
+  if (chunk.startsWith(DELTA_LIST_BY_TWO)) listBy = 2;
+  if (listBy) chunk = chunk.slice(1);
+  const blocks = chunk.split('x');
 
-    if (listBy) {
-        if (blocks.length === 1) {
-            tokens = wrap(chunk, listBy);
+  if (listBy) {
+    if (blocks.length === 1) {
+      tokens = wrap(chunk, listBy);
+    } else {
+      let items = [];
+      items = blocks.map((block, i) => (wrap(block, listBy)));
+      items.forEach((item, i) => {
+        if (i > 0) {
+          const c = tokens.pop();
+          for (let j = 0; j < c; j++) tokens.push(item[0]);
+          item = item.slice(1);
         }
-        else {
-            let items = [];
-            items = blocks.map((block, i) => (wrap(block, listBy)));
-            items.forEach((item, i) => {
-                if (i > 0) {
-                    let c = tokens.pop();
-                    for (let j = 0; j < c; j++)
-                        tokens.push(item[0]);
-                    item = item.slice(1);
-                }
-                tokens.push(...item);
-            });
-            return tokens;
-
-            // let nums = wrap(blocks[1], listBy);
-            // let num = _getInt(nums[0]);
-            // let list = [];
-            // for (let i = 0; i < blocks[0]; i++) {
-            //     items.push(num);
-            // }
-            // items.push(...nums.slice(1));
-            //
-            // let tokens = [];
-            // blocks.forEach((block) => items = items.concat(wrap(block,listBy)));
-            // items.forEach((block, i) => {
-            //     if (i > 0) {
-            //         let c = items.pop();
-            //         for (let j=0; j < c; j++) items.push(block[0]);
-            //         block = block.split(1);
-            //     }
-            //     items.push(...block);
-            // });
-        }
+        tokens.push(...item);
+      });
+      return tokens;
     }
-    else if (blocks.length === 2) {
-        let num = parseInt(blocks[1], intBase);
-        for (let i = 0; i < blocks[0]; i++) {
-            tokens.push(num);
-        }
+  } else if (blocks.length === 2) {
+    const num = parseInt(blocks[1], intBase);
+    for (let i = 0; i < blocks[0]; i++) {
+      tokens.push(num);
     }
-    else tokens = [parseInt(chunk, intBase)];
+  } else tokens = [parseInt(chunk, intBase)];
 
-    return tokens;
+  return tokens;
 };
 
 /**
@@ -189,24 +161,22 @@ const parseDeltaChunks = chunk => {
  * @param string for split into chunks
  * @return [] of chunks
  */
-const deltaChunks = string => {
-    let chunks = [],
-        buf = '';
-    for (let ltr of string) {
-        if (ltr === NUM_DELIMITER) {
-            (buf!=='') && chunks.push(buf);
-            buf = '';
-        }
-        else if (ltr === DELTA_LIST_BY_ONE || ltr === DELTA_LIST_BY_TWO) {
-            (buf!=='') && chunks.push(buf);
-            buf = ltr;
-        }
-        else {
-            buf += ltr;
-        }
+const deltaChunks = (string) => {
+  const chunks = [];
+  let buf = '';
+  for (const ltr of string) {
+    if (ltr === NUM_DELIMITER) {
+      (buf !== '') && chunks.push(buf);
+      buf = '';
+    } else if (ltr === DELTA_LIST_BY_ONE || ltr === DELTA_LIST_BY_TWO) {
+      (buf !== '') && chunks.push(buf);
+      buf = ltr;
+    } else {
+      buf += ltr;
     }
-    if (buf !== '') chunks.push(buf);
-    return chunks;
+  }
+  if (buf !== '') chunks.push(buf);
+  return chunks;
 };
 
 /**
@@ -217,11 +187,11 @@ const deltaChunks = string => {
  * @returns {[]} list of several strings
  */
 const wrap = (string, count) => {
-    let list = [];
-    for (let i = 0; i<string.length; i+=count) {
-        list.push(parseInt(string.slice(i, i+count), intBase));
-    }
-    return list;
+  const list = [];
+  for (let i = 0; i < string.length; i += count) {
+    list.push(parseInt(string.slice(i, i + count), intBase));
+  }
+  return list;
 };
 
 
@@ -256,7 +226,3 @@ const wrap = (string, count) => {
 //     Strings with base of numbers
 // decode('x16;3,f');
 // decode('x2;11,101');
-
-
-
-
--- scripts/encode.js
+++ scripts/encode.js
@@ -9,17 +9,17 @@ const MODE_DELTA_STRING = 2;
  * @return {string} encoding string
  */
 const encode = (tokens, mode = MODE_SIMPLE_STRING) => {
-    if (!tokens || !Array.isArray(tokens)) {
-        console.error('tokens argument should be an array of integers');
-    }
-    if (tokens.length === 0) return '';
-    if (mode === MODE_SIMPLE_STRING) {
-        return(encodeString(tokens));
-    }
-    else if (mode === MODE_DELTA_STRING) {
-        return(encodeDelta(tokens));
-    }
-    else console.error('you must select 1 or 2 at second parameter (1 - simple string, 2 - delta string)');
+  if (!tokens || !Array.isArray(tokens)) {
+    console.error('tokens argument should be an array of integers');
+  }
+  if (tokens.length === 0) return '';
+  if (mode === MODE_SIMPLE_STRING) {
+    return (encodeString(tokens));
+  }
+  if (mode === MODE_DELTA_STRING) {
+    return (encodeDelta(tokens));
+  }
+  console.error('you must select 1 or 2 at second parameter (1 - simple string, 2 - delta string)');
 };
 
 /**
@@ -28,33 +28,33 @@ const encode = (tokens, mode = MODE_SIMPLE_STRING) => {
  * @param tokens
  * @return {string} compressed string
  */
-const encodeString = tokens => {
-    if (tokens.length < 3) {
-        return tokens.join(',');
+const encodeString = (tokens) => {
+  if (tokens.length < 3) {
+    return tokens.join(',');
+  }
+  const min = tokens[0];
+  const sortedTokens = tokens.sort((a, b) => a - b);
+
+  let compressedString = `${min}`;
+  let rangeStart = false;
+  let values = [];
+
+  sortedTokens.forEach((id, index) => {
+    const newId = id - min;
+
+    if (rangeStart === false && sortedTokens[index + 1] === id + 1) {
+      rangeStart = newId;
+    } else if (rangeStart === false && sortedTokens[index + 1] !== id + 1) {
+      values.push(newId);
+    } else if (rangeStart !== false && sortedTokens[index + 1] !== id + 1) {
+      values.push([rangeStart, newId]);
+      rangeStart = false;
     }
-    const min = tokens[0];
-    const sortedTokens = tokens.sort((a, b) => a - b);
+  });
 
-    let compressedString = `${min}`;
-    let rangeStart = false;
-    let values = [];
-
-    sortedTokens.forEach((id, index) => {
-        const newId = id - min;
-
-        if (rangeStart === false && sortedTokens[index + 1] === id + 1) {
-            rangeStart = newId;
-        } else if (rangeStart === false && sortedTokens[index + 1] !== id + 1) {
-            values.push(newId);
-        } else if (rangeStart !== false && sortedTokens[index + 1] !== id + 1) {
-            values.push([rangeStart, newId]);
-            rangeStart = false;
-        }
-    });
-
-    values = values.map(item => (typeof item === 'number' ? item : `${item[0]}-${item[1]}`));
-    compressedString = `${compressedString}(${values.join(',')})`;
-    return compressedString;
+  values = values.map((item) => (typeof item === 'number' ? item : `${item[0]}-${item[1]}`));
+  compressedString = `${compressedString}(${values.join(',')})`;
+  return compressedString;
 };
 
 /**
@@ -63,11 +63,11 @@ const encodeString = tokens => {
  * @param tokens
  * @return {string} compressed string
  */
-const encodeDelta = tokens => {
-    const sortedTokens = _deltaCompression(tokens);
-    const chunks = _xBlocks(sortedTokens);
-    const compressedString = _compressToString(chunks);
-    return compressedString;
+const encodeDelta = (tokens) => {
+  const sortedTokens = _deltaCompression(tokens);
+  const chunks = _xBlocks(sortedTokens);
+  const compressedString = _compressToString(chunks);
+  return compressedString;
 };
 
 /**
@@ -77,13 +77,13 @@ const encodeDelta = tokens => {
  * @return {[]} sorted array of difference tokens
  * @private
  */
-const _deltaCompression = tokens => {
-    tokens.sort();
-    let diffTokens = [];
-    tokens.forEach((token, i) => {
-        i !== 0 ? diffTokens.push(token-tokens[i-1]) : diffTokens.push(token);
-    });
-    return diffTokens;
+const _deltaCompression = (tokens) => {
+  tokens.sort();
+  const diffTokens = [];
+  tokens.forEach((token, i) => {
+    i !== 0 ? diffTokens.push(token - tokens[i - 1]) : diffTokens.push(token);
+  });
+  return diffTokens;
 };
 
 /**
@@ -93,17 +93,17 @@ const _deltaCompression = tokens => {
  * @return {[]}
  * @private
  */
-const _xBlocks = tokens => {
-    let buf = [],
-        tokensCopy = [];
-    tokens.forEach((token, i) => {
-        if (i !== 0 && (token !== tokens[i - 1] || i === tokens.length - 1)) {
-            buf.length > 1 ? tokensCopy.push(`${buf.length}x${buf[0]}`) : tokensCopy.push(`${buf[0]}`);
-            buf = [token];
-        } else buf.push(token);
-    });
-    if (buf.length !== 0) tokensCopy.push(`${buf}`);
-    return tokensCopy;
+const _xBlocks = (tokens) => {
+  let buf = [];
+  const tokensCopy = [];
+  tokens.forEach((token, i) => {
+    if (i !== 0 && (token !== tokens[i - 1] || i === tokens.length - 1)) {
+      buf.length > 1 ? tokensCopy.push(`${buf.length}x${buf[0]}`) : tokensCopy.push(`${buf[0]}`);
+      buf = [token];
+    } else buf.push(token);
+  });
+  if (buf.length !== 0) tokensCopy.push(`${buf}`);
+  return tokensCopy;
 };
 
 /**
@@ -113,29 +113,29 @@ const _xBlocks = tokens => {
  * @return {string} compressedString
  * @private
  */
-const _compressToString = chunks => {
-    let del = ',',
-        newDel = null,
-        compressedString = '';
+const _compressToString = (chunks) => {
+  let del = ',';
+  let newDel = null;
+  let compressedString = '';
 
-    chunks.forEach((chunk) => {
-        if (chunk.indexOf('x') > -1) {
-            let [x, val] = chunk.split('x');
-            if (x.length === val.length) {
-                newDel = (val.length===1 ? '.' : (val.length===2 ? ':' : ','));
-            } else {
-                newDel = ',';
-            }
-        } else {
-            newDel = (chunk.length===1 ? '.' : (chunk.length===2 ? ':' : ','));
-        }
-        if (newDel === ',' || newDel !== del) {
-            compressedString += newDel;
-        }
-        compressedString += chunk;
-        del = newDel;
-    });
-    return `~${compressedString}`;
+  chunks.forEach((chunk) => {
+    if (chunk.indexOf('x') > -1) {
+      const [x, val] = chunk.split('x');
+      if (x.length === val.length) {
+        newDel = (val.length === 1 ? '.' : (val.length === 2 ? ':' : ','));
+      } else {
+        newDel = ',';
+      }
+    } else {
+      newDel = (chunk.length === 1 ? '.' : (chunk.length === 2 ? ':' : ','));
+    }
+    if (newDel === ',' || newDel !== del) {
+      compressedString += newDel;
+    }
+    compressedString += chunk;
+    del = newDel;
+  });
+  return `~${compressedString}`;
 };
 
 
@@ -144,10 +144,8 @@ const _compressToString = chunks => {
 // encode([1]);
 // encode([1, 2]);
 // encode([1, 2, 3]);
-// encode([1, 2, 3, 4]);
+console.log(encode([1, 2, 3, 4]));
 // encode([1, 3, 5, 7]);
 // encode([1, 2, 3, 5, 6, 7]);
-encode([1, 2, 3, 5, 6, 7, 9], 1);
-encode([1, 2, 3, 5, 6, 7, 9], 2);
-
-
+console.log(encode([1, 2, 3, 5, 6, 7, 9], 1));
+console.log(encode([1, 2, 3, 5, 6, 7, 9], 2));
