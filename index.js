fe2b70e5154825edc8a85573593203892761f66b
bukina.e <ecfhf567>
2019-12-20 18:53:31 +0500

    create functions encode, decode for interface

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const Encode = require('./scripts/encode');
+const Decode = require('./scripts/decode');
 
+const encode = (tokens, mode) => new Encode().parse(tokens, mode);
+const decode = (string) => new Decode().parse(string);
+
+exports.encode = encode;
+exports.decode = decode;
