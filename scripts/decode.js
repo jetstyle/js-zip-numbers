2592262bcd5e3d5ac38d696455188c9a389f01f1
bukina.e <ecfhf567>
2019-12-20 14:44:54 +0500

    fix by eslint

--- scripts/decode.js
+++ scripts/decode.js
@@ -49,6 +49,7 @@ class Decode {
     let buff = ''; const tokens = []; const
       zipBuff = [];
 
+    // eslint-disable-next-line no-restricted-syntax
     for (const ltr of string) {
       if (ltr === this.ZIP_START_DELIMITER) zipBuff.push(1);
       if (ltr === this.ZIP_END_DELIMITER) zipBuff.pop();
@@ -78,7 +79,10 @@ class Decode {
     let tokens = [];
     const chunks = this._deltaChunks(string);
 
-    chunks.forEach((chunk) => tokens = tokens.concat(this._parse_deltaChunks(chunk)));
+    chunks.forEach((chunk) => {
+      tokens = tokens.concat(this._parseDeltaChunks(chunk));
+      return tokens;
+    });
 
     let last = 0;
     tokens.forEach((token, i) => {
@@ -98,6 +102,7 @@ class Decode {
   _parseToken(token) {
     let tokens = [];
     if (token.indexOf(this.ZIP_START_DELIMITER) > -1) {
+      // eslint-disable-next-line prefer-const
       let [base, subString] = token.split(this.ZIP_START_DELIMITER);
       base = parseInt(base, 10);
       const items = this.parseString(subString.slice(0, subString.length - 1));
@@ -122,7 +127,7 @@ class Decode {
    * @param chunk
    * @return {[]} array with tokens
    */
-  _parse_deltaChunks(chunk) {
+  _parseDeltaChunks(chunk) {
     let listBy;
     let tokens = [];
     if (chunk.startsWith(this.DELTA_LIST_BY_ONE)) listBy = 1;
@@ -139,7 +144,7 @@ class Decode {
         items.forEach((item, i) => {
           if (i > 0) {
             const c = tokens.pop();
-            for (let j = 0; j < c; j++) tokens.push(item[0]);
+            for (let j = 0; j < c; j += 1) tokens.push(item[0]);
             item = item.slice(1);
           }
           tokens.push(...item);
@@ -148,7 +153,7 @@ class Decode {
       }
     } else if (blocks.length === 2) {
       const num = parseInt(blocks[1], this.intBase);
-      for (let i = 0; i < blocks[0]; i++) {
+      for (let i = 0; i < blocks[0]; i += 1) {
         tokens.push(num);
       }
     } else tokens = [parseInt(chunk, this.intBase)];
@@ -165,6 +170,7 @@ class Decode {
   _deltaChunks(string) {
     const chunks = [];
     let buf = '';
+    // eslint-disable-next-line no-restricted-syntax
     for (const ltr of string) {
       if (ltr === this.NUM_DELIMITER) {
         (buf !== '') && chunks.push(buf);
@@ -195,37 +201,5 @@ class Decode {
     return list;
   }
 }
-
-// Tests
-// Done
-// decode('123');
-// decode('123,456');
-// decode('1-3');
-// decode('1-3,5-9');
-// decode('120(0,3,5,8,9)');
-// decode('12(1,4),140(0,2,5)');
-// decode('120(0,3,6),130-132');
-// decode('120(0-6)');
-// decode('~155');
-// decode('~1,2,3');
-// decode('~1');
-// decode('~.1');
-// decode('~.123');
-// decode('~.123:1012');
-// decode('~.12:10.45');
-// decode('~.12:10.45,146,234.14');
-// decode('~3x1');
-// decode('1-10');
-// decode('1,2,5,7-13');
-// decode('14(0-8,18)');
-// decode('~.13x3');
-// decode('~.2x12');
-// decode('~12,3x4,');
-// decode('~.54x13:1010x11');
-
-//  In process
-//     Strings with base of numbers
-// decode('x16;3,f');
-// decode('x2;11,101');
 
 module.exports = Decode;
